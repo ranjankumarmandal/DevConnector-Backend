e089ec8faf57a9e041e7eab4d1157b1cabe42f98
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-12-04 17:02:20 +0530

    created config folder and connected MongoDB with mongoose in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB(); // connect database - MongoDB
 app.get('/', (req, res) =>
   res.send('API Running (made by Ranjan Kumar Mandal)...')
 );
