52a72e74b77aa059ef2fbc894f2db955f555c5d5
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-12-05 21:17:21 +0530

    completed this whole backend application

--- routes/api/users.js
+++ routes/api/users.js
@@ -69,7 +69,7 @@ router.post(
       );
     } catch (error) {
       console.error(error.message);
-      res.status(500).send('Server error');
+      res.status(500).send('Server Error');
     }
   }
 );
